753ce2ee28db94a08545d710ff9d1d7ed3c4fa07
sanyam <sanyamvirmani111@gmail.com>
2022-05-03 02:23:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NewsApp
+
+## Built With
+
+- **React**
+
+### News based on different categories
+
+- Bussiness
+- Entertainment
+- Health
+- Science
+- Sports
+- Technology
+
+### Dependencies
+
+- react-infinite-scroll-component
+- react-top-loading-bar
+
+# Installation
+
+- `npm install`
+- create `.env.local` file and create your api key from https://newsapi.org/
+  - `REACT_APP_NEWS_API="apikey"` in `.env.local` file
+- npm run start
---
+++ src/components/NewsItem.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className=" badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            !imageUrl
+              ? "https://static.toiimg.com/thumb/msid-88254851,width-1070,height-580,imgsize-60892,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-danger">
+              By {author ? author : "unknown"} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a href={newsUrl} target="blank" className="btn btn-sm btn-dark">
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
